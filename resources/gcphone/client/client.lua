b958c5ce22990fef0df9510be41ab37662bff0d6
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-08 00:36:39 -0800

    Try two at the DPAD UP

--- resources/gcphone/client/client.lua
+++ resources/gcphone/client/client.lua
@@ -11,7 +11,7 @@ local KeyToucheCloseEvent = {
     { code = 176, event = 'Enter' },
     { code = 177, event = 'Backspace' },
 }
-local KeyOpenClose = 27 -- DPAD UP  // OLD: --288 -- F2
+local KeyOpenClose = 172 -- DPAD UP (you have to hold it a bit-- love jay)  // OLD: --288 -- F2
 local KeyTakeCall = 38 -- E
 local menuIsOpen = false
 local contacts = {}
