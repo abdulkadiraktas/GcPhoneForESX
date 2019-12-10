d26630c656434d1d78bf699f4e3a57bf45e8112f
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-09 20:02:43 -0800

    LATEST

--- resources/gcphone/client/client.lua
+++ resources/gcphone/client/client.lua
@@ -137,7 +137,7 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
 
   	    if menuIsOpen == true then
-            DisableControlAction(2, 45)
+            DisableControlAction(0, 45)
             if IsDisabledControlPressed(0, 45) then
                 TooglePhone()
             else
