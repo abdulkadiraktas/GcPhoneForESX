3e2858e8562b3905d2d085314249bd4bd28ab887
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-09 20:29:32 -0800

    LATEST

--- resources/gcphone/client/client.lua
+++ resources/gcphone/client/client.lua
@@ -131,17 +131,15 @@ end)
 -- bring up the phone
 --====================================================================================
 local keypressTimer = 0 -- don't change this... it needs to start at 0
-local keypressThreshold = 130 -- each 100 is about 1 second ... 200 = ~2 Seconds
+local keypressThreshold = 100 -- each 100 is about 1 second ... 200 = ~2 Seconds
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
   	    if menuIsOpen == true then
-            print ("menu open")
             DisableControlAction(0, 263, true)
             DisableControlAction(0, 140, true)
             if IsDisabledControlJustPressed(0, 45) then
-                print("pushed B")
                 TooglePhone()
                 EnableControlAction(0, 263, true)
                 EnableControlAction(0, 140, true)
